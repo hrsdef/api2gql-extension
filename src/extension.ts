bbb52f3c7c2f7b866ce8591e5f4b827e245c6457
haoyu1 <haoyu1@kingsoft.com>
2024-11-28 15:31:30 +0800

    fix: import query mutation类型

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,7 @@ import { TypeDefsGenerator } from './utils/typeDefsGenerator';
 import { ApiDoc } from './utils/types';
 
 const getResolverImports = (apiSpec: ApiDoc) => [
-    "import { Query, Ctx, Args, Resolver } from 'type-graphql';",
+    "import { Query, Mutation, Ctx, Args, Resolver } from 'type-graphql';",
     `import { RequestParams, Res${apiSpec.API_NAME} } from './typeDefs';`,
     "import { requestAPI } from '../../utils/request';",
     "import { Context } from 'overlord-server';"
